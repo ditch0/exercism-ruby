e6ba995c5ebded2b9fb41786d7d728e383dceb94
ditch0 <ditch0@users.noreply.github.com>
2017-05-27 23:50:34 +0300

    Refactor space-age

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,5 +1,5 @@
 class SpaceAge
-  ORBITAL_PERIODS = {
+  planets_orbital_periods = {
     earth:   1,
     mercury: 0.2408467,
     venus:   0.61519726,
@@ -8,48 +8,16 @@ class SpaceAge
     saturn:  29.447498,
     uranus:  84.016846,
     neptune: 164.79132
-  }.freeze
+  }
+
+  planets_orbital_periods.each do |planet, period|
+    define_method "on_#{planet}" do
+      @age_in_seconds / 60 / 60 / 24 / 365.25 / period
+    end
+  end
 
   def initialize(age_in_seconds)
     @age_in_seconds = age_in_seconds
-  end
-
-  def on_earth
-    on_planet(:earth)
-  end
-
-  def on_mercury
-    on_planet(:mercury)
-  end
-
-  def on_venus
-    on_planet(:venus)
-  end
-
-  def on_mars
-    on_planet(:mars)
-  end
-
-  def on_jupiter
-    on_planet(:jupiter)
-  end
-
-  def on_saturn
-    on_planet(:saturn)
-  end
-
-  def on_uranus
-    on_planet(:uranus)
-  end
-
-  def on_neptune
-    on_planet(:neptune)
-  end
-
-  private
-
-  def on_planet(planet)
-    @age_in_seconds / 60 / 60 / 24 / 365.25 / ORBITAL_PERIODS[planet]
   end
 end
 
