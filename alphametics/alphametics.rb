1d046f8a3c6d4e5482db065a783d4f4a7d32c51e
ditch0 <ditch0@users.noreply.github.com>
2017-06-01 23:16:31 +0300

    Refactor alphametics

--- alphametics/alphametics.rb
+++ alphametics/alphametics.rb
@@ -1,24 +1,43 @@
-module Alphametics
+class Alphametics
+  attr_reader :equation
+
   def self.solve(equation)
-    variables = extract_variables(equation)
+    new(equation).solve
+  end
+
+  def initialize(equation)
+    @equation = equation
+  end
+
+  def solve
     (0..9).to_a.permutation(variables.count) do |values|
       variables_values = Hash[variables.zip values]
-      return variables_values if correct_solution?(equation, variables_values)
+      return variables_values if correct_solution?(variables_values)
     end
     {}
   end
 
-  def self.correct_solution?(equation, variables_values)
-    solution = equation.gsub(Regexp.union(variables_values.keys), variables_values)
+  def correct_solution?(variables_values)
+    solution = equation.gsub(variables_regexp, variables_values)
     return false if solution =~ /(^| )[0]+\d/
     eval(solution)
   end
 
-  def self.extract_variables(equation)
+  private
+
+  def variables
+    @variables ||= extract_variables
+  end
+
+  def extract_variables
     equation
       .gsub(/\W/, '')
       .chars
       .uniq
+  end
+
+  def variables_regexp
+    @variables_regexp ||= Regexp.union(variables)
   end
 end
 
